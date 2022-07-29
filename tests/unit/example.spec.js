243733f692feda35856079a33fad2cfa93280af2
Guillermo <guillermoberna_@hotmail.com>
2022-07-29 00:08:51 -0400

    Prueba Unitaria simple

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,16 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+//estamos haciendo un prueba unitaria simple, en este caso de que Let debe ser mayor o igual a 10
+//en la documentacion de jest esta casi todos los tipos de prueba que necesitemos
+//dependiendo del paquete que hayamos instalado, puedes iniciar test con "npm run test:unit"
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+
+describe("Example Component", ()=>{
+  test("Debe de ser mayor a 10",()=>{
+    //Arreglo
+    let value = 10;
+    //Estimulo
+    value = value + 2
+    //Observar el resultado
+    expect( value ).toBeGreaterThan(10)
   })
+  
 })
