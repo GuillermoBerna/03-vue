721485413152ae7614051bcfe0ebc96360b0ca1c
Guillermo <guillermoberna_@hotmail.com>
2022-07-29 17:53:47 -0400

    Probando titulo snapshot

--- tests/unit/components/Counter.spec.js
+++ tests/unit/components/Counter.spec.js
@@ -3,11 +3,21 @@ import {shallowMount} from "@vue/test-utils"//se importa una herramienta de vue 
 import Counter from "@/components/Counter"//ademas tenemos que importar el componente desde la carpeta components
 
 describe ("Counter Component", ()=>{
-    test("Debe hacer match con el snapshot", ()=>{
+    //test("Debe hacer match con el snapshot", ()=>{
        
-        const wrapper = shallowMount(Counter)//dentro de esta funcion de js, se uso shallowmount 
-        expect(wrapper.html()).toMatchSnapshot()//y aca de saca un snapshot y compararlo con el renderizado de hmtl y estos deben coincidir
+    //    const wrapper = shallowMount(Counter)//dentro de esta funcion de js, se uso shallowmount 
+    //    expect(wrapper.html()).toMatchSnapshot()//y aca de saca un snapshot y compararlo con el renderizado de hmtl y estos deben coincidir
+    //}
+
+    test("h2 debe tener el valor por defecto 'Counter' ", ()=>{
+
+        const wrapper = shallowMount(Counter)
+
+        const h2 = wrapper.find("h2")
+
+        expect(h2.text()).toBe("Counter")
+
     })
 })
 
-//se crear un
+
